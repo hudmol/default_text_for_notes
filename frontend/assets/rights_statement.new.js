480deddfc25bf2f405a6e0788d2d4dcedf037906
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2017-09-05 20:50:14 +0200

    Attempt to update to 2.1.1
    This attempts to update to work with the permissions stuff in v2.1.1.
    Not totally sure if this is the best way or if this is what users
    actually want...but maybe?

--- frontend/assets/rights_statement.new.js
+++ frontend/assets/rights_statement.new.js
@@ -1,12 +1,29 @@
 $(function() {
   var initRSForm = function(subform) {
+      
+      let initNote = function(label, text) { 
+        let note_exist = false;
+        $('[id*="rights_statement_notes"] input[id*="_label"]', subform).each(function() { 
+            if ( $(this).val() == label ) { note_exist = true };
+         });
+        
+        if ( !note_exist ) { 
+          setTimeout( function() { $('[id*="rights_statement_notes"] .add-note', subform ).click() }, 1000 ) 
+          let note = $('.notes-form', subform); 
+          setTimeout( function() { $('ul:first > li:last select.top-level-note-type > option', note ).filter(function(){ 
+            return $(this).text() == 'Additional Information'; 
+           }).prop('selected', true ); $('ul:first > li:last select.top-level-note-type', note).trigger('change'); }, 1000 ); 
+          setTimeout( function() {  $('ul:first > li:last input[id*="_label"]', note ).val(label) }, 1000 ) 
+          setTimeout( function() { $("ul:first > li:last textarea[id*='_rights_statements__']:last", note).data('CodeMirror').setValue(text); }, 1000);
+        }
+      }
+      
+      let dfd = $.Deferred();
       $("[name$='[rights_type]']", subform).change(function(event) {
-	      SETUP_TEXTAREA($("textarea[id*='_rights_statements__'][id*='__permissions_']"),
-			     DEFAULT_TEXT_FOR_NOTES__RIGHTS_STATEMENT_PERMISSIONS);
-	      
-	      SETUP_TEXTAREA($("textarea[id*='_rights_statements__'][id*='__restrictions_']"),
-			     DEFAULT_TEXT_FOR_NOTES__RIGHTS_STATEMENT_RESTRICTIONS);
-	  });
+        dfd.done( setTimeout( function() { initNote("Permissions", DEFAULT_TEXT_FOR_NOTES__RIGHTS_STATEMENT_PERMISSIONS ) }, 1000 ))
+          .then ( initNote("Restrictions", DEFAULT_TEXT_FOR_NOTES__RIGHTS_STATEMENT_RESTRICTIONS ) )
+      });
+       
   };
 
   $(document).bind("subrecordcreated.aspace", function(event, object_name, subform) {
